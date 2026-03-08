ad4c3d81ab33f0d33c75c4ca719258f7b751da42
basundhara-070 <2022ugec070@nitjsr.ac.in>
2026-03-08 13:14:47 +0530

    update backend url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  // baseURL: "https://daily-tracker-5c52.onrender.com/api",
-   baseURL: "http://localhost:5000/api"
+   baseURL: "https://daily-tracker-5c52.onrender.com/api",
+   //baseURL: "http://localhost:5000/api"
 });
 
 export default API;
